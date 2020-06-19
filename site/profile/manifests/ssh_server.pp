867b88993d1ee6bc94503a55b59fc724003b90be
curtisn1 <57296369+curtisn1@users.noreply.github.com>
2020-06-19 16:16:49 +0100

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDDGO7t2O5MaYz7hl3ICKU5T/pZ7YYQusHmpxmno62NMX7D0aSoBlWWUpZ/Jxdi/00QKZszwCYgWeQwZSzlcoZH8k6xCRhHA9Bv33uQJO650+miEQzm2EN5yixxPJ7WaYkz42el4A3vVL+DpGJ9BYuEglUbVb+3WaJP1joLO6b3lwmsUJ2L8YvZMLvQ0eXGVg2XKxBrFVZTD3nJWSnRNIrub99KEsNLB4ePlXNTpzN3TAcBer4BXd1gSHW/GzM3o91wILVOtGIlTllc7wv4w6Lrs9wWrycubQ2fEU7Hwk8i53ZuhncAVLaMXn2YvbdyRQgAEYuDg1uu0i6jwb4+f3G',
 	}  
 }
