02447deaabccf84f9ca1fc975f9284d3562e1e6e
curtisn1 <57296369+curtisn1@users.noreply.github.com>
2020-06-19 15:34:58 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,6 +2,10 @@ node default {
 }
 node 'ip-192-168-0-247.eu-north-1.compute.internal' {
   include role::master_server
+  file { '/root/README':
+    ensure => file,
+    content => $fqdn,
+   }
  }
  node /^web/ {
   include role::app_server
